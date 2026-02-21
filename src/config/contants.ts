3c3ff61f433b4df23471ec4db9dc77ccb8ac8d34
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2026-02-21 20:03:50 -0300

    Add MY_REPO_URL constant: Include repository URL in constants for improved configuration and accessibility.

--- src/config/contants.ts
+++ src/config/contants.ts
@@ -1,4 +1,5 @@
 const MY_EMAIL = "gallogarcianicolas@gmail.com";
+const MY_REPO_URL = "https://github.com/galoss88/app-test";
 const BASE_URL_API =
   "https://botfilter-h5ddh6dye8exb7ha.centralus-01.azurewebsites.net";
 const sufixUrlCandidate =
@@ -9,6 +10,7 @@ const sufixApplyPosition = "/api/candidate/apply-to-job";
 export {
   BASE_URL_API,
   MY_EMAIL,
+  MY_REPO_URL,
   sufixApplyPosition,
   sufixUrlCandidate,
   sufixUrlPositions,
