70ae456acb8904c8b6fcebb1a003bf290a3d00ab
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2026-02-21 18:55:35 -0300

    Enhance Title component: Add default styling to the Title component, improving visual consistency and allowing for custom styles through props.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,6 +2,14 @@ interface ITitleProp {
   title?: string;
   props?: React.HTMLAttributes<HTMLHeadingElement>;
 }
-export const Title = ({ title, ...props }: ITitleProp) => {
-  return <h1 {...props}>{title}</h1>;
+export const Title = ({ title, ...rest }: ITitleProp) => {
+  return (
+    <h1 style={{ ...defaultStyle, ...rest.props?.style }} {...rest}>
+      {title}
+    </h1>
+  );
+};
+
+const defaultStyle: React.CSSProperties = {
+  fontSize: "1.2rem",
 };
