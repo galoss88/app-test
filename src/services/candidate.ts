275f78d64b7487f2fd839bb6d404be6d577b90bb
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2026-02-21 19:50:58 -0300

    Refactor getCandidate function: Introduce IResponseApiGetCandidate interface for type safety, enhance error handling for API response, and ensure consistent error messaging.

--- src/services/candidate.ts
+++ src/services/candidate.ts
@@ -1,11 +1,23 @@
 import { BASE_URL_API, MY_EMAIL, sufixUrlCandidate } from "../config/contants";
 
-export const getCandidate = async () => {
+interface IResponseApiGetCandidate {
+  uuid: string;
+  candidateId: string;
+  applicationId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+export const getCandidate = async (): Promise<IResponseApiGetCandidate> => {
   const sufix = `${sufixUrlCandidate}?email=${MY_EMAIL}`;
   try {
-    await fetch(`${BASE_URL_API}${sufix}`, {
+    const responseApi = await fetch(`${BASE_URL_API}${sufix}`, {
       method: "GET",
     });
+    if (!responseApi.ok) {
+      throw new Error(`Error HTTP: ${responseApi.status}`);
+    }
+    return responseApi.json();
   } catch (e) {
     console.log(`Error al obtener información del candidato: ${e}`);
     throw new Error("Ocurrio un error al obtener información del candidato.");
