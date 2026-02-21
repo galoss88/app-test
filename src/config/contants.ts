b22248984d0b62982770dbfd29e3737a42e612a5
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2026-02-21 19:07:56 -0300

    Update constants: Add new API suffix URLs for candidate and job position retrieval, and ensure consistent formatting of existing constants.

--- src/config/contants.ts
+++ src/config/contants.ts
@@ -1,5 +1,8 @@
-const MY_EMAIL = 'gallogarcianicolas@gmail.com'
-const BASE_URL_API = "https://botfilter-h5ddh6dye8exb7ha.centralus-01.azurewebsites.net"
+const MY_EMAIL = "gallogarcianicolas@gmail.com";
+const BASE_URL_API =
+  "https://botfilter-h5ddh6dye8exb7ha.centralus-01.azurewebsites.net";
+const sufixUrlCandidate =
+  "/api/candidate/get-by-email?email=gallogarcianicolas@gmail.com";
+const sufixUrlPositions = "/api/jobs/get-list";
 
-
-export { MY_EMAIL, BASE_URL_API }
+export { BASE_URL_API, MY_EMAIL, sufixUrlCandidate, sufixUrlPositions };
