4d0329963d1d6d9293a184dcc9e403027c114d1f
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2026-02-21 12:57:37 -0300

    Enhance Card component: Update the Card component to include styling and introduce a new CardItem subcomponent for displaying titles.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,27 @@ interface CardProps {
   children: React.ReactNode;
 }
 
+interface ICardItemProps {
+  title: string;
+}
+
 export const Card = ({ children }: CardProps) => {
-  return <div>{children}</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        padding: "10px",
+        borderRadius: "10px",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
+
+
+const CardItem = ({ title }: ICardItemProps) => {
+  return <div>{title}</div>;
+};
+
+Card.Item = CardItem;
