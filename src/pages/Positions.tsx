420a58876ad5fda951934fdc540fa9be3c0aa6e7
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2026-02-21 13:10:30 -0300

    Update Positions component: Add custom styles to Card for improved appearance and user experience.

--- src/pages/Positions.tsx
+++ src/pages/Positions.tsx
@@ -2,7 +2,13 @@ import { Card } from "../components/Card";
 
 export const Positions = () => {
   return (
-    <Card>
+    <Card
+      style={{
+        padding: "10px",
+        borderRadius: "10px",
+        boxShadow: "0 0 1px 1px grey",
+      }}
+    >
       <Card.Item title="Hello World" />
     </Card>
   );
