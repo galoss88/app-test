7a2edc08f0b06b89b762e654da6070cc7418e457
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2026-02-21 13:54:49 -0300

    Update Positions component: Integrate useGetPositions hook to fetch and display job positions dynamically, enhancing the UI with a list of position cards that include title, input, and submit button.

--- src/pages/Positions.tsx
+++ src/pages/Positions.tsx
@@ -1,16 +1,32 @@
 import { Card } from "../components";
+import { useGetPositions } from "../hooks/useGetPositions";
+import type { IPositionsApi } from "../types/types";
 
 export const Positions = () => {
+  const { positions, loading, error } = useGetPositions();
   return (
-    <Card
-      style={{
-        padding: "10px",
-        borderRadius: "10px",
-        boxShadow: "0 0 1px 1px grey",
-      }}
-    >
-      <Card.Item title="Hello World" />
-      <Card.Item.Input type="text"></Card.Item.Input>
-    </Card>
+    <div>
+      {positions &&
+        positions.length > 0 &&
+        positions?.map((position: IPositionsApi) => (
+          <Card
+            key={position.id}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              borderRadius: "10px",
+              boxShadow: "0 0 1px 1px grey",
+              gap: "10px",
+            }}
+          >
+            <Card.Item title={position.title} />
+            <Card.Item.Input type="text"></Card.Item.Input>
+            <Card.Item.Button title="Enviar postulación.">
+              Submit
+            </Card.Item.Button>
+          </Card>
+        ))}
+    </div>
   );
 };
