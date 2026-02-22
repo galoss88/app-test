2feefe477c04eec66453bed9b8c27bdad0bec089
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2026-02-22 13:04:01 -0300

    Refactor Positions component to use PositionCard: Replace Card component with PositionCard for improved structure and functionality in displaying job positions and application interactions.

--- src/pages/Positions.tsx
+++ src/pages/Positions.tsx
@@ -1,8 +1,8 @@
-import { Card } from "../components";
 import { Loading } from "../components/Loading";
 import { useApplyPosition } from "../hooks/useApplyPosition";
 import { useGetPositions } from "../hooks/useGetPositions";
 import type { IPositionsApi } from "../types/types";
+import { PositionCard } from "./positions/PositionCard";
 
 export const Positions = () => {
   const { positions, loading, error } = useGetPositions();
@@ -23,31 +23,11 @@ export const Positions = () => {
       {positions &&
         positions.length > 0 &&
         positions?.map((position: IPositionsApi) => (
-          <Card
+          <PositionCard
             key={position.id}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              padding: "10px",
-              borderRadius: "10px",
-              boxShadow: "0 4px 20px -2px rgba(0, 0, 0, 0.1)",
-              border: "1px solid rgba(0, 0, 0, 0.146)",
-              gap: "10px",
-            }}
-          >
-            <Card.Item.Title title={position.title} />
-            <Card.Item.Input
-              placeholder="Github repository URL"
-              type="text"
-            ></Card.Item.Input>
-            <Card.Item.Button
-              title="Enviar postulación."
-              onClick={() => applyPosition({ jobId: position.id })}
-            >
-              Submit
-            </Card.Item.Button>
-          </Card>
+            position={position}
+            applyPosition={applyPosition}
+          />
         ))}
     </div>
   );
